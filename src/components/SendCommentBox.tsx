6d0c90efd8f92abc94849d587575b6baddcd208e
Nafiseh Ebrahimi <ebranafas@gmail.com>
2022-09-14 23:56:59 +0430

    handled addcomment method (main comment)

--- src/components/SendCommentBox.tsx
+++ src/components/SendCommentBox.tsx
@@ -1,9 +1,33 @@
-import React from "react";
+import { FormEvent, useContext, useRef } from "react";
 import profile from "../assets/avatars/image-amyrobson.webp";
+import { CommentListContext } from "../context/CommentContext";
+import { CurrentUser } from "../data";
+import { CreateId } from "../helper";
 
 function SendCommentBox() {
+  const UseCommentContext = useContext(CommentListContext);
+  const commentRef = useRef<HTMLTextAreaElement>(null);
+
+  const AddComment = (e: FormEvent) => {
+    e.preventDefault();
+    if(commentRef.current?.value !== "") {
+      UseCommentContext.setCommentList([
+        ...UseCommentContext.commentList,
+        {
+          id: Number(CreateId(5)),
+          date: new Date().toString(),
+          message: commentRef.current?.value || "comment",
+          profileImage: CurrentUser.profileImage,
+          username: CurrentUser.username,
+          rate: 0,
+          parentId: null
+        }
+      ])
+    }
+  }
+
   return (
-    <form action="" className="bg-white flex flex-col lg:flex-row items-start py-5 px-6 rounded-lg my-4">
+    <form action="" onSubmit={(e) => AddComment(e)} className="bg-white flex flex-col lg:flex-row items-start py-5 px-6 rounded-lg my-4">
       <div className="w-10 h-10 overflow-hidden rounded-full relative hidden lg:block">
         <img
           src={profile}
@@ -11,7 +35,7 @@ function SendCommentBox() {
           alt="your profile"
         />
       </div>
-      <textarea className="w-full lg:w-[80%] outline-none border-2 focus:border-purple leading-7 mx-auto bg-transparent resize-none p-3  border-gray-200 rounded-md" placeholder="Add a comment..."></textarea>
+      <textarea ref={commentRef} className="w-full lg:w-[80%] outline-none border-2 focus:border-purple leading-7 mx-auto bg-transparent resize-none p-3  border-gray-200 rounded-md" placeholder="Add a comment..."></textarea>
       <button type="submit" className="bg-purple hidden lg:block text-white rounded-md py-3 px-7 font-semibold hover:opacity-70">SEND</button>
       <div className="mt-4 w-full flex lg:hidden items-center justify-between">
         <div className="w-10 h-10 overflow-hidden rounded-full relative">
--- src/container/CommentContainer.tsx
+++ src/container/CommentContainer.tsx
@@ -4,29 +4,31 @@ import { CommentListContext } from "../context/CommentContext";
 
 function CommentContainer() {
   const UseCommentContext = useContext(CommentListContext);
-	
-	const handleReplies = (id: number) => {
-		return UseCommentContext.commentList.filter(beforeComment => beforeComment.parentId === id);
-	}
+
+  const handleReplies = (id: number) => {
+    return UseCommentContext.commentList.filter(
+      (beforeComment) => beforeComment.parentId === id
+    );
+  };
 
   return (
     <div>
       {UseCommentContext.commentList.map((comment, index) => {
-				if(comment.parentId === null) {
-					return (
-						<Comment
-								key={index}
-								avatar={comment.profileImage}
-								username={comment.username}
-								message={comment.message}
-								date={comment.date}
-								replies={handleReplies(comment.id)}
-								replyId={null}
-								rate={comment.rate}
-							/>
-					)
-				} 
-			})}
+        if (comment.parentId === null) {
+          return (
+            <Comment
+              key={index}
+              avatar={comment.profileImage}
+              username={comment.username}
+              message={comment.message}
+              date={comment.date}
+              replies={handleReplies(comment.id)}
+              replyId={null}
+              rate={comment.rate}
+            />
+          );
+        }
+      })}
     </div>
   );
 }
